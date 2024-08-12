6302809c206743a8b3c0f76a01c6513fd13a068c
Guenther <guenther.zoppelt@gmail.com>
2024-08-12 09:28:49 +0100

    create app structure

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # proxy
 docker container with nginx as reverse proxy router to qwik app frontend, express backend and postgresql database
+
+#### Trying to implement
+[Qwik JS - How to Build a Full Stack SAAS Application](https://www.youtube.com/playlist?list=PLkswEDcfBXYcl1gW7L5zyCVF9LpGhlOqu)
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,25 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Hi 👋</h1>
+      <div>
+        Can't wait to see what <div class="text-2xl">you</div> build with qwik!
+        <br />
+        Happy coding.
+      </div>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
