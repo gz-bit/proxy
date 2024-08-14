f5e28d3d347c80958603a045c395cc65e29dc684
Guenther <guenther.zoppelt@gmail.com>
2024-08-14 11:54:18 +0100

    supabase .env concept

--- frontend/src/utils/supabase.ts
+++ frontend/src/utils/supabase.ts
@@ -1,7 +1,16 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js"
 import { Sb } from './supabase.env'
 
-const supabaseUrl = Sb.project_url
-const supabaseAnonPublic = Sb.key_anon_public
+const env = import.meta.env.DEV ? Sb.dev : Sb.prod
 
-export const supabase = createClient(supabaseUrl, supabaseAnonPublic);
+export const supabase = createClient(
+  env.project_url,
+  env.key_anon_public
+)
+
+// Supabase projects are paused if not accessed for a week
+// to keep the dev project "Page Local" open in a prod environment:
+if (import.meta.env.PROD) {
+  const devClient = createClient(Sb.dev.project_url, Sb.dev.key_anon_public)
+  devClient.auth.signOut() // we will see if this is sufficient
+}
